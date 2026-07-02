23d8b74989d9e277fa508103d27d2acceada8ef2
hxl495github <56458928+hxl495github@users.noreply.github.com>
2026-07-02 15:42:40 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/violet.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Yuki",
+	bio: "",
 	links: [
 		{
 			name: "Twitter",
